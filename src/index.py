3ef6a0dfde26161b67a014c37a9aee75ee3d9585
Vitaly Zorin <addefan@mail.ru>
2024-12-05 14:57:00 +0300

    feat(function): add handling text message

--- src/index.py
+++ src/index.py
@@ -3,7 +3,7 @@ from os import getenv
 
 import requests
 
-from src.texts import WELCOME
+from src.texts import WELCOME, CANT_ANSWER
 
 SUCCESS_RESPONSE = {
     "statusCode": 200,
@@ -24,9 +24,49 @@ def send_message(reply_text, input_message):
     requests.post(url=url, json=data)
 
 
-def handle_message(message):
+def get_answer_from_gpt(question, iam_token):
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+
+    headers = {
+        "Accept": "application/json",
+        "Authorization": f"Bearer {iam_token}",
+    }
+
+    data = {
+        "modelUri": f"gpt://{getenv("FOLDER_ID")}/yandexgpt",
+        "messages": [
+            {"role": "system", "text": "Ответь на экзаменационный билет."},
+            {"role": "user", "text": question},
+        ],
+    }
+
+    response = requests.post(url=url, headers=headers, json=data)
+    if response.status_code != 200:
+        return None
+
+    alternatives = response.json()["result"]["alternatives"]
+    final_alternatives = list(filter(
+        lambda alternative: alternative["status"] == "ALTERNATIVE_STATUS_FINAL",
+        alternatives,
+    ))
+
+    if not final_alternatives:
+        return None
+
+    answer = final_alternatives[0]["message"].get("text")
+    return answer
+
+
+def handle_message(message, iam_token):
     if (text := message.get("text")) and text in {"/start", "/help"}:
         send_message(WELCOME, message)
+    elif text := message.get("text"):
+        answer = get_answer_from_gpt(text, iam_token)
+        if not answer:
+            send_message(CANT_ANSWER, message)
+            return
+
+        send_message(answer, message)
 
 
 def handler(event, context):
@@ -36,6 +76,6 @@ def handler(event, context):
     if not message:
         return SUCCESS_RESPONSE
 
-    handle_message(message)
+    handle_message(message, context.token["access_token"])
 
     return SUCCESS_RESPONSE

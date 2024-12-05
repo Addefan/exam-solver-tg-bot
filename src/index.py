c1e1cef01f6f47240bbf40f6a2c19642c9326d8d
Vitaly Zorin <addefan@mail.ru>
2024-12-05 17:08:36 +0300

    refactor(function): take out handling text and photo messages to separate functions

--- src/index.py
+++ src/index.py
@@ -10,33 +10,40 @@ SUCCESS_RESPONSE = {
 }
 
 
+def handle_text_message(text, message, iam_token):
+    answer = get_answer_from_gpt(text, iam_token)
+    if not answer:
+        send_message(CANT_ANSWER, message)
+        return
+
+    send_message(answer, message)
+
+
+def handle_photo_message(tg_photo, message, iam_token):
+    image_id = tg_photo[-1]["file_id"]
+    image_path = get_file_path(image_id)
+    image = get_image(image_path)
+    text = recognize_text(encode_to_base64(image), iam_token)
+    if not text:
+        send_message(CANT_ANSWER, message)
+        return
+
+    handle_text_message(text, message, iam_token)
+
+
 def handle_message(message, iam_token):
     if (text := message.get("text")) and text in {"/start", "/help"}:
         send_message(WELCOME, message)
-    elif text := message.get("text"):
-        answer = get_answer_from_gpt(text, iam_token)
-        if not answer:
-            send_message(CANT_ANSWER, message)
-            return
 
-        send_message(answer, message)
+    elif text := message.get("text"):
+        handle_text_message(text, message, iam_token)
+
     elif message.get("photo") and message.get("media_group_id"):
         send_message(CAN_HANDLE_ONLY_ONE_PHOTO, message)
+
     elif image := message.get("photo"):
-        image_id = image[-1]["file_id"]
-        image_path = get_file_path(image_id)
-        image = get_image(image_path)
-        text = recognize_text(encode_to_base64(image), iam_token)
-        if not text:
-            send_message(CANT_ANSWER, message)
-            return
+        handle_photo_message(image, message, iam_token)
 
-        answer = get_answer_from_gpt(text, iam_token)
-        if not answer:
-            send_message(CANT_ANSWER, message)
-            return
-
-        send_message(answer, message)
     else:
         send_message(CAN_HANDLE_ONLY_TEXT_OR_PHOTO, message)
 
